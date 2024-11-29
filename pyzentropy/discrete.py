8c5cbd5be2518b524c6a3b754775d4ab60e0c714
lukeamyers <lam7027@psu.edu>
2024-11-29 10:10:47 -0500

    new `discrete.py` file

---
+++ pyzentropy/discrete.py
@@ -0,0 +1,36 @@
+from scipy import constants
+import numpy as np
+from functools import cached_property
+from scipy.interpolate import UnivariateSpline
+
+BOLTZMANN_CONSTANT = constants.physical_constants[
+    "Boltzmann constant in eV/K"
+][0]
+
+
+class DiscreteV:
+    def __init__(
+        self,
+        volume: np.array
+    ):
+        self.value = value
+        self.volume = volume
+class DiscreteT:
+    def __init__(
+        self,
+        value: np.array,
+        temperature: np.array
+    ):
+        self.value = value
+        self.temperature = temperature
+
+class DiscreteVT
+    def __init__(
+        self,
+        value: np.array,
+        volume: np.array,
+        temperature: np.array
+    ):
+        self.value = value
+        self.volume = volume
+        self.temperature = temperature
