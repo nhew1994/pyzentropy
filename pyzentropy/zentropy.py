61c991a82624e3914bd780711926e13ff27e932f
lukeamyers <lam7027@psu.edu>
2024-12-07 15:27:43 -0500

    zero temperature zk

--- pyzentropy/zentropy.py
+++ pyzentropy/zentropy.py
@@ -69,11 +69,7 @@ class Configuration:
         else:
             # validate consistency of the provided values
             pass
-
-        self.partition_function = self.multiplicity*np.exp(
-            -helmholtz_energy/(BOLTZMANN_CONSTANT*temperature[:, np.newaxis])
-        )
-
+        
         heat_caps = np.zeros(entropy.shape)
         for i in range(entropy.shape[1]):
             s_spline = UnivariateSpline(temperature, entropy[:, i], s=0, k=1)
@@ -100,17 +96,35 @@ class System:
                     f"Configurations with mismatched {attribute}s: {mismatched[1]}")
         self.volume = configurations[0].volume
         self.temperature = configurations[0].temperature
+        
+        t_zero = np.where(self.temperature == 0) 
+        ground_energy_k = [config.helmholtz_energy[t_zero,:] for config in configurations]
+        self.ground_state_energy = np.min(ground_energy_k)
 
+        self.f_lowest = np.min([config.helmholtz_energy for config in configurations])
+        for config in configurations:
+            #shifting by the lowest helmholtz energy is important because of the exponential
+            f_shifted = config.helmholtz_energy - self.f_lowest
+            kb = BOLTZMANN_CONSTANT
+            w = config.multiplicity
+            t = config.temperature[:, np.newaxis]
+            out = np.full_like(f_shifted, np.inf)
+            np.divide(f_shifted, kb*t, out=out, where=t!=0)
+            # z will become inf when temperature is zero if f_shifted has
+            # negative values, but it won't if f_shifted is shifted by the 
+            # lowest helmholtz energy. z is zero when temperature is zero.
+            # make sure that the probabilities are not effected by this later.
+            config.partition_function = w*np.exp(-out) 
         self.partition_function = np.sum(
             [config.partition_function for config in configurations],axis=0
         )
 
-        self.helmholtz_energy = -BOLTZMANN_CONSTANT*self.temperature[:, np.newaxis]*np.log(
-            self.partition_function
-        )
-
         for config in configurations:
             config.probability = config.partition_function / self.partition_function
+    
+        self.helmholtz_energy = -BOLTZMANN_CONSTANT*self.temperature[:, np.newaxis]*np.log(
+            self.partition_function # might be wrong with shifted helmholtz values in configs
+        )
 
         pk_list = [
             config.partition_function / self.partition_function
